8b75c32fee6d3cd2ab669197f87aab67640b5f4f
Dani Cumplido <danicumplido04@gmail.com>
2026-02-25 23:40:21 +0100

    Creada pag dashboard como herramienta temporal de desarrollo

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import { useAuth } from "../hooks/useAuth";
+import { Link } from "react-router";
 
 function MainLayout() {
     const { user } = useAuth();
@@ -11,9 +12,23 @@ function MainLayout() {
                 <h2 className="mb-8 text-2xl font-bold">QA Workspace</h2>
                 <nav>
                     <ul className="space-y-4">
-                        <li className="text-slate-300">Dashboard</li>
-                        <li className="text-slate-300">Proyectos</li>
-                        <li className="text-slate-300">Equipo</li>
+                        <li className="text-slate-300">
+                            <Link to="/dashboard" className="font-semibold text-indigo-400 hover:text-indigo-300">
+                                Dashboard
+                            </Link>
+                        </li>
+                        <li className="text-slate-300">
+                            <Link to="/proyectos" className="font-semibold text-indigo-400 hover:text-indigo-300">
+                                Proyectos
+                            </Link>
+                        </li>
+                        {user?.role === "ADMIN" && (
+                            <li className="text-slate-300">
+                                <Link to="/equipo" className="font-semibold text-indigo-400 hover:text-indigo-300">
+                                    Equipo
+                                </Link>
+                            </li>
+                        )}
                     </ul>
                 </nav>
             </aside>
@@ -23,7 +38,7 @@ function MainLayout() {
                 {/* HEADER */}
                 <header className="flex items-center justify-end h-16 px-8 bg-white border-b">
                     <div className="flex items-center space-x-4">
-                        <span className="text-sm font-medium text-gray-600"><span>{user?.email}   ({user?.role})</span></span>
+                        <span className="text-sm font-medium text-gray-600"><span>{user?.full_name} ({user?.role})</span></span>
                         <div className="w-8 h-8 bg-blue-100 rounded-full"></div>
                     </div>
                 </header>
